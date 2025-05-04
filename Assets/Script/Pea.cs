3e923d2224e59cef710edfdae1c7c5aa3c50aef9
Kim JunHa <jjun2205@gmail.com>
2025-05-04 22:39:16 +0900

    Add CompleteTriat Logic
    완전 우성 형질 기본 로직 작성 및 priavate scene

--- Assets/Script/Pea.cs
+++ Assets/Script/Pea.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Pea : Plant
 {
@@ -10,14 +11,27 @@ public class Pea : Plant
         base.Initialize(gridNumber, parent1, parent2);
     }
 
-    public override void InitializeCompleteTrait(Dictionary<CompleteTraitType, float> parent1, Dictionary<CompleteTraitType, float> parent2)
+    public override void InitializeCompleteTrait(Dictionary<CompleteTraitType, int> parent1, Dictionary<CompleteTraitType, int> parent2)
     {
         base.InitializeCompleteTrait(parent1, parent2);
+
+        foreach (CompleteTraitType trait in Enum.GetValues(typeof(CompleteTraitType)))
+        {
+            if (trait == CompleteTraitType.None)
+                break;
+            if (completeGenetics[trait] == 0)
+                completeResistances[trait] = 0.9f;
+            else
+                completeResistances[trait] = 0.5f;
+        }
+
+        // 저항력 계산 및 삽입 필요. 지금은 러프하게 열성 만족하면 0.9 저항력 가지도록
     }
 
     public override void InitializeIncompleteTrait(Dictionary<IncompleteTraitType, float> parent1, Dictionary<IncompleteTraitType, float> parent2)
     {
         base.InitializeIncompleteTrait(parent1, parent2);
+        // 저항력 계산 및 삽입 필요
     }
 
 
--- Assets/Script/Plant.cs
+++ Assets/Script/Plant.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public enum CompleteTraitType // 기존 형질
 {
@@ -15,7 +16,7 @@ public enum CompleteTraitType // 기존 형질
 
 public enum IncompleteTraitType // 불완전 형질
 {
-
+    None
 }
 
 public enum WaveType
@@ -26,7 +27,10 @@ public enum WaveType
     Pest,
     Cold,
     HeavyRain,
+    None
 }
+
+// 형질이나 웨이브 추가 시 GetResistantValue 및 번식 시 Initialize Trait 에서 저항력 계산 추가 필요.
 
 public abstract class Plant : MonoBehaviour
 {
@@ -35,15 +39,18 @@ public abstract class Plant : MonoBehaviour
 
     [SerializeField]
     protected Dictionary<CompleteTraitType, float> completeResistances = new Dictionary<CompleteTraitType, float>(); // 기존 우성 법칙 따르는 형질의 저항력
+    
+    [SerializeField]
+    protected Dictionary<CompleteTraitType, int> completeGenetics= new Dictionary<CompleteTraitType, int>(); // int 값은 우성 개수
 
     [SerializeField]
-    protected Dictionary<IncompleteTraitType, float> IncompleteResistances = new Dictionary<IncompleteTraitType, float>(); // 불완전 우성 따르는 형질 저항력
+    protected Dictionary<IncompleteTraitType, float> incompleteResistances = new Dictionary<IncompleteTraitType, float>(); // 불완전 우성 따르는 형질 저항력
 
     public virtual void Initialize(int gridNumber, Plant parent1, Plant parent2) // 번식 event 발생 시킬 때 instanciate하고 호출
     {
         InitializeGridPosition(gridNumber);
-        InitializeCompleteTrait(parent1.completeResistances, parent2.completeResistances);
-        InitializeIncompleteTrait(parent1.IncompleteResistances, parent2.IncompleteResistances);
+        InitializeCompleteTrait(parent1.completeGenetics, parent2.completeGenetics);
+        InitializeIncompleteTrait(parent1.incompleteResistances, parent2.incompleteResistances);
 
     }
 
@@ -52,9 +59,48 @@ public abstract class Plant : MonoBehaviour
         // grid번호 따라 포지션 계산해서 grid Position 초기화
     }
 
-    public virtual void InitializeCompleteTrait(Dictionary<CompleteTraitType, float> parent1, Dictionary<CompleteTraitType, float> parent2)
+    public virtual void InitializeCompleteTrait(Dictionary<CompleteTraitType, int> parent1, Dictionary<CompleteTraitType, int> parent2) // 우성 열성만 여기서 계산. 실제 저항력은 하위 객체에서 상속 받아서 저장
     {
-        // 유전 법칙 따라 저항력 계산 + 식물 종류 따라 다른 계산 식 필요
+        foreach (CompleteTraitType trait in Enum.GetValues(typeof(CompleteTraitType)))
+        {
+            if (trait == CompleteTraitType.None)
+                break;
+
+            parent1.TryGetValue(trait, out int p1Count);
+            parent2.TryGetValue(trait, out int p2Count);
+
+            int allele1;
+            if (p1Count == 2) // RR
+            {
+                allele1 = 1;
+            }
+            else if (p1Count == 0) // rr
+            {
+                allele1 = 0;
+            }
+            else // Rr
+            {
+                allele1 = UnityEngine.Random.Range(0, 2);
+            }
+
+            int allele2;
+            if (p2Count == 2) // RR
+            {
+                allele2 = 1;
+            }
+            else if (p2Count == 0) // rr
+            {
+                allele2 = 0;
+            }
+            else // Rr
+            {
+                allele2 = UnityEngine.Random.Range(0, 2);
+            }
+
+            completeGenetics[trait] = allele1 + allele2;
+        }
+
+        // 유전 법칙 따라 저항력 계산은 식물마다 다르니 하위 객체에서 상속받아 계산 
     }
 
     public virtual void InitializeIncompleteTrait(Dictionary<IncompleteTraitType, float> parent1, Dictionary<IncompleteTraitType, float> parent2)
@@ -62,11 +108,9 @@ public abstract class Plant : MonoBehaviour
         // 불완전 유전 법칙 따라 저항력 계산 + 식물 종류 따라 다른 계산 식 필요
     }
 
-    public void CanResist(WaveType wave)
+    public void CanResist(WaveType wave) // if can't resist, Call Die()
     {
-        // 웨이브 형식 따라 특성 보고 버틸 확률 계산
-
-        int randomNumber = Random.Range(0, 100);
+        int randomNumber = UnityEngine.Random.Range(0, 100);
         if(randomNumber <= (int) (GetResistanceValue(wave) * 100))
         {
             return;
@@ -79,8 +123,19 @@ public abstract class Plant : MonoBehaviour
 
     protected float GetResistanceValue(WaveType wave)
     {
-        //wave 따른 버틸 확률 반환해주...게 하고 싶은데 enum만 추가해도 알아서 추가되도록 만들고 싶은데...
-        return 0;
+        float resistance = 0f;
+        switch (wave)
+        {
+            case WaveType.Wind: resistance = completeResistances[CompleteTraitType.WindResistance]; break;
+            case WaveType.Flood: resistance = completeResistances[CompleteTraitType.FloodResistance]; break;
+            case WaveType.Pest: resistance = completeResistances[CompleteTraitType.PestResistance]; break;
+            case WaveType.Cold: resistance = completeResistances[CompleteTraitType.ColdResistance]; break;
+            case WaveType.HeavyRain: resistance = completeResistances[CompleteTraitType.HeavyRainResistance]; break;
+            case WaveType.Aging: resistance = completeResistances[CompleteTraitType.NaturalDeath]; break;
+            // 특성 추가되면 추가
+        }
+        return resistance;
+
     }
 
     public void Die()
