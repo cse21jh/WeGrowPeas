b190561ac2217662b06ed3e474953bdf958d5dd9
Yoon <cse21jh@korea.ac.kr>
2025-05-06 17:40:15 +0900

    Add peaprefab, grid initial setting

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Grid : MonoBehaviour
 {
+    List<Plant> plants = new List<Plant>();
+    private int maxPlantNum = 16;
+
+    [SerializeField] private GameObject peaPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < 2; i++)
+        {
+            GameObject obj = Instantiate(peaPrefab);
+            Pea pea = obj.GetComponent<Pea>();
+            //pea.Initialize();
+            plants.Add(pea);
+        }
     }
 
     // Update is called once per frame
@@ -18,6 +30,19 @@ public class Grid : MonoBehaviour
 
     public IEnumerator Breeding()
     {
+        //40초 동안 아래 과정 반복 진행 가능
+
+        //교배할 부모 완두콩 두 개 선택
+
+        //자식 완두콩 형질 계산 후 Instantiate
+
+        //Grid 리로드
+
         yield return null;
     }    
+
+    private void ShowPlantsOnGrid()
+    {
+
+    }
 }
