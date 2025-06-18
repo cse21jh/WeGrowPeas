d010642fc96d133090c1c1ef33ccb308e04dd594
Kim JunHa <jjun2205@gmail.com>
2025-06-18 22:48:39 +0900

    Add BGM
    BGM 추가

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -18,6 +18,7 @@ public class GameManager : Singleton<GameManager>
     // Start is called before the first frame update
     void Start()
     {
+        SoundManager.Instance.PlayBgm("Farm");
         StageUpdate();
         StartCoroutine(GameStart());
     }
--- Assets/Scripts/Core/SoundManager.cs
+++ Assets/Scripts/Core/SoundManager.cs
@@ -31,7 +31,7 @@ public class SoundManager : Singleton<SoundManager>
             EffectSoundDictionary.Add(audioclip.name, audioclip);
         }
 
-        BGMVolume = 1f;
+        BGMVolume = 0.05f;
         EffectVolume = 0.3f;
 
         EffectSoundDictionary.Add("SelectPlant", Resources.Load<AudioClip>("Audio/CasualGameSounds/DM-CGS-21"));
@@ -39,12 +39,13 @@ public class SoundManager : Singleton<SoundManager>
         //EffectSoundDictionary.Add("GetItem", Resources.Load<AudioClip>("Audio/CasualGameSounds/DM-CGS-45"));
         
         EffectSoundDictionary.Add("KillBug", Resources.Load<AudioClip>("Audio/Shapeforms Audio Free Sound Effects/PUNCH_DESIGNED_HEAVY_23"));
-        
+        EffectSoundDictionary.Add("Farm", Resources.Load<AudioClip>("Audio/BGM/BGM"));
+
         //EffectSoundDictionary.Add("Farming", Resources.Load<AudioClip>("Audio/BGM/farming_¾û¶×ÇÑ ÀÛ´ç¸ðÀÇ"));
         //EffectSoundDictionary.Add("Magician Cave", Resources.Load<AudioClip>("Audio/BGM/magician_´Ï°¡ ¸ÕÀú ÇßÀÝ¾Æ (Short)"));
         //EffectSoundDictionary.Add("Cake Shop", Resources.Load<AudioClip>("Audio/BGM/Shop_Fluffing a Duck"));
         //EffectSoundDictionary.Add("TutorialScene", Resources.Load<AudioClip>("Audio/BGM/tutorial_LP1607180062_ÀÌÇý¸°_Tongtong"));
-        
+
         //EffectSoundDictionary.Add("MainMenu", EffectSoundDictionary["Cake Shop"]);
     }
 
