b6f7e7c1a85abc5e9cc817d000dfdb18af1a0b5a
Yoon <cse21jh@korea.ac.kr>
2025-06-18 22:07:42 +0900

    Add scene loader

---
+++ Assets/Scripts/Core/SceneLoader.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader Instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void LoadGardenScene()
+    {
+        SceneManager.LoadScene("Garden");
+    }
+
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene("GameOverScene");
+    }
+}
