f72dc1658f1ec73d3a25d7409dad7c0bc1ab0699
Kim JunHa <jjun2205@gmail.com>
2025-07-31 14:57:17 +0900

    Add Bugs
    중심을 기준으로 대칭적인 위치로 일직선으로 움직이는 벌레를 하나 추가했습니다
    - 목표 지점에 도달하면 알아서 삭제됩니다 (저항력 증가는 되지 않음)
    
    농장 내부를 무작위로 움직이는 벌레가 추가되었습니다
    - 베지어 곡선으로 정말 다 무작위로 넣어서 저도 예측 안됨 ㅎ
    - 플레이 해보면서 보정 하겠습니다

---
+++ Assets/Scripts/Bug/RandomMovingBug.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovingBug : Bug
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override IEnumerator Moving()
+    {
+        yield return StartCoroutine(base.Moving());
+        
+        float time = 0;
+
+        Vector2 startPos = transform.position;
+        Vector2 targetPos = SetRandomPos();
+        Vector2 controlPos1 = SetRandomPos();
+        Vector2 controlPos2 = SetRandomPos();
+
+        float distSelfToTarget;
+
+        while (true)
+        {
+            time += Time.deltaTime / 10 * speed;
+
+            distSelfToTarget = Vector2.Distance(transform.position, targetPos);
+
+            if (distSelfToTarget < 0.5f)
+            {
+                time = 0;
+
+                startPos = transform.position;
+                targetPos = SetRandomPos();
+                controlPos1 = SetRandomPos();
+                controlPos2 = SetRandomPos();
+            }
+
+            Vector2 position1 = Vector2.Lerp(startPos, controlPos1, time);
+            Vector2 position2 = Vector2.Lerp(controlPos1, controlPos2, time);
+            Vector2 position3 = Vector2.Lerp(controlPos2, targetPos, time);
+
+            Vector2 position4 = Vector2.Lerp(position1, position2, time);
+            Vector2 position5 = Vector2.Lerp(position2, position3, time);
+
+            MoveToward(Vector2.Lerp(position4, position5, time));
+
+            yield return null;
+        }
+    }
+
+    private Vector2 SetRandomPos()
+    {
+        return new Vector2(Random.Range(-9.0f, 9.0f), Random.Range(-5.0f, 5.0f));
+    }
+
+    protected override IEnumerator HitBug()
+    {
+        yield return StartCoroutine(base.HitBug());
+        yield return StartCoroutine(KillBug());
+    }
+
+    protected override IEnumerator KillBug()
+    {
+        yield return StartCoroutine(base.KillBug());
+    }
+}
---
+++ Assets/Scripts/Bug/StraightMovingBug.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightMovingBug : Bug
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override IEnumerator Moving()
+    {
+        yield return StartCoroutine(base.Moving());
+        Vector2 targetPos = -transform.position;
+
+        float distSelfToTarget = Vector2.Distance(transform.position, targetPos);
+
+        while (distSelfToTarget > 0.5f)
+        {
+            MoveToward(targetPos);
+            yield return null;
+            distSelfToTarget = Vector2.Distance(transform.position, targetPos);
+        }
+        Destroy(this.gameObject);
+    }
+
+    protected override IEnumerator HitBug()
+    {
+        yield return StartCoroutine(base.HitBug());
+        yield return StartCoroutine(KillBug());
+    }
+
+    protected override IEnumerator KillBug()
+    {
+        yield return StartCoroutine(base.KillBug());
+    }
+}
