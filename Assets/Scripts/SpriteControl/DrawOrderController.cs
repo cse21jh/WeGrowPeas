a6168ee021d5a4e271c3804d2dc2f16df90713f4
Ping-guin <stanchoi2015@gmail.com>
2025-08-15 01:40:39 +0900

    Add BG Components & Increased Pea Prefab Size
    - 배경에 몇가지 물체를 추가했습니다.
    - 가시성을 위해 완두콩 프리팹의 사이즈를 키우고, 생성되는 위치를 조정했습니다.

--- Assets/Scripts/SpriteControl/DrawOrderController.cs
+++ Assets/Scripts/SpriteControl/DrawOrderController.cs
@@ -4,6 +4,7 @@ public class DrawOrderController : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
     [SerializeField] private float drawOrderOffset = 0f;
+    [SerializeField] private float YOffset = 0f;
 
     private void Awake()
     {
@@ -12,6 +13,7 @@ public class DrawOrderController : MonoBehaviour
 
     private void Start()
     {
+        transform.position = new Vector3(transform.position.x, transform.position.y + YOffset, transform.position.z);
         spriteRenderer.sortingOrder = Mathf.RoundToInt(transform.position.y * -100f + drawOrderOffset);
     }
 }
