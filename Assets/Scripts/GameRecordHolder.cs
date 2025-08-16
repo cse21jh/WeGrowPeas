e07c2f37534716566fd45cedf53bad0e5215a958
Yoon <cse21jh@korea.ac.kr>
2025-08-16 18:30:03 +0900

    Add player rank

--- Assets/Scripts/GameRecordHolder.cs
+++ Assets/Scripts/GameRecordHolder.cs
@@ -10,11 +10,39 @@ public static class GameRecordHolder
 
     public static int[] TotalWaveKilled { get; private set; }
 
+    public static string PlayerRank { get; private set; }
+
     public static void SaveRecord(int stage, int peas, int bugs, int[] killed)
     {
         maxStageReached = stage - 1;
         TotalPeas = peas;
         TotalBugsKilled = bugs;
         TotalWaveKilled = (int[])killed.Clone();
+
+        CalculateRank();
+    }
+
+    private static void CalculateRank()
+    {
+        if (TotalPeas < 144)
+        {
+            PlayerRank = "B";
+            return;
+        }
+        else if (TotalPeas < 240)
+        {
+            PlayerRank = "A";
+            return;
+        }
+        else if (TotalPeas < 336)
+        {
+            PlayerRank = "S";
+            return;
+        }
+        else
+        {
+            PlayerRank = "SS";
+            return;
+        }
     }
 }
