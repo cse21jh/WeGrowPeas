f63047753ccbb375a89e2c1f5b45d13b1cf0d33a
Ping-guin <stanchoi2015@gmail.com>
2025-08-27 19:32:28 +0900

    Update RT Size Every Frame
    - RTResizeController가 매 프레임 작동되게 했습니다.

--- Assets/Scripts/RTResizeController.cs
+++ Assets/Scripts/RTResizeController.cs
@@ -8,9 +8,15 @@ public class RTResizeController : MonoBehaviour
     [SerializeField] private RenderTexture[] rtArray;
     [SerializeField] private Camera[] camArray;
 
+    private int previousWidth;
+    private int previousHeight;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        previousWidth = Screen.width;
+        previousHeight = Screen.height;
+
         StartCoroutine(DelayResizeRT(0.1f));
     }
 
@@ -49,6 +55,18 @@ public class RTResizeController : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.R))
         {
             ResizeRenderTextures();
+        }
+
+
+        if (Screen.width != previousWidth || Screen.height != previousHeight)
+        {
+            // 해상도 변경 감지 시 로직 실행
+            Debug.Log($"해상도 변경 감지: {Screen.width}x{Screen.height}");
+            ResizeRenderTextures();
+
+            // 예: UI 재배치, 카메라 조정 등
+            previousWidth = Screen.width;
+            previousHeight = Screen.height;
         }
     }
 }
