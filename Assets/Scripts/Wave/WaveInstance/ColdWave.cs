9c4e6ca9cf808b0c12bd8cb211a4103ec1a45da2
Kim JunHa <jjun2205@gmail.com>
2025-08-28 20:11:01 +0900

    Fix Bug
    추위 웨이브가 바람 웨이브와 텍스트가 동일하던 버그를 수정했습니다

--- Assets/Scripts/Wave/WaveInstance/ColdWave.cs
+++ Assets/Scripts/Wave/WaveInstance/ColdWave.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ColdWave : Wave
 {
     public override WaveType WaveType => WaveType.Cold;
-    public override string WaveDescription => "거센 바람이 몰아칩니다......";
+    public override string WaveDescription => "기온이 떨어지고 있습니다.....";
     public override string WaveSoundString => "Cold";
     public override int UnlockStage => 20;
 }
