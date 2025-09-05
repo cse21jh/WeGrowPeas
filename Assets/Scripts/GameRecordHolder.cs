f8d5c43bab230354a5fc63a7bbb42eafedd03343
Yoon <cse21jh@korea.ac.kr>
2025-09-06 01:43:36 +0900

    Add something
    밑에 커밋한 거 덜 올림

--- Assets/Scripts/GameRecordHolder.cs
+++ Assets/Scripts/GameRecordHolder.cs
@@ -6,11 +6,22 @@ public static class GameRecordHolder
 {
     public static int maxStageReached { get; private set; }
     public static int TotalPeas { get; private set; }
+    public static int TotalPeanuts { get; private set; }
+    public static int soldPeas { get; private set; }
+    public static int soldPeanuts { get; private set; }
     public static int TotalBugsKilled { get; private set; }
-
+    public static int totalGoldEarned { get; private set; }
+    public static int totalGoldSpend { get; private set; }
     public static int[] TotalWaveKilled { get; private set; }
+    
+    //가장 많이 산 아이템
 
+    //최다 판매 식물
+
+    //기타 로그들
     public static string PlayerRank { get; private set; }
+
+    
 
     public static void SaveRecord(int stage, int peas, int bugs, int[] killed)
     {
--- Assets/Scripts/UI/UIGameRecord.cs
+++ Assets/Scripts/UI/UIGameRecord.cs
@@ -9,6 +9,14 @@ public class UIGameRecord : MonoBehaviour
     [SerializeField] private TextMeshProUGUI textPea;
     [SerializeField] private TextMeshProUGUI textBug;
 
+    private string[] endingText = new string[4]
+    {
+        "살아남는 데는 성공했지만, 식량 확보가 보장되지 않는다고 판단되어 다른 대안을 탐색하러 떠났다…",
+        "안정적인 환경에서 생산이 빠른 좋은 식량으로 평가받아 환경적인 변화가 크게 없는 일부 지역에서 쓰이게 되었다.",
+        "다양한 환경에서 괜찮은 생산량을 보여 주었기에 비상시에 사용될 대체식품으로 각광받으며 좋은 먹거리가 되었다.",
+        "뛰어난 적응성과 번식 속도를 입증해 전세계에 확산되었고, 이후 인류의 핵심적인 식량이 되었다!"
+    };
+
     // Start is called before the first frame update
     void Start()
     {
