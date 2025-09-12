de553fa217e24535279a5184644d36e7e278328c
Kim JunHa <jjun2205@gmail.com>
2025-09-13 00:58:00 +0900

    Fix UnstoppableBug
    멈추지 않는 벌레가 이제 별 먹은 것 처럼 색이 무지갯빛으로 왔다갔다 합니다

--- Assets/Scripts/Bug/UnstoppableBug.cs
+++ Assets/Scripts/Bug/UnstoppableBug.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class UnstoppableBug : Bug
 {
+    public SpriteRenderer bodySprite;
+
+    private Color[] targetColors = new Color[]
+        {
+        new Color(255/255f, 0/255f, 0/255f),
+        new Color(255/255f, 0/255f, 255/255f),
+        new Color(0/255f, 0/255f, 255/255f),
+        new Color(0/255f, 255/255f, 255/255f),
+        new Color(0/255f, 255/255f, 0/255f),
+        new Color(255/255f, 255/255f, 0/255f)
+        };
+
+    private int currentColorIndex = 0;
+    private float colorValue = 1f;
+
     protected override void Start()
     {
         base.Start();
+        bodySprite = transform.Find("RoachSprite").transform.Find("roach_body").GetComponent<SpriteRenderer>();
+        StartCoroutine(ChangeColor());
     }
 
     protected override IEnumerator Moving()
@@ -36,6 +53,45 @@ public class UnstoppableBug : Bug
     public override IEnumerator KillBug()
     {
         yield return StartCoroutine(base.KillBug());
+    }
+
+    private IEnumerator ChangeColor()
+    {
+        bodySprite.color = Color.red;
+
+        while (true)
+        {
+            int nextColorIndex = (currentColorIndex + 1) % targetColors.Length;
+            Color targetColor = targetColors[nextColorIndex];
+            Color currentColor = bodySprite.color;
+
+            while (Mathf.Abs(currentColor.r - targetColor.r) + Mathf.Abs(currentColor.g - targetColor.g) + Mathf.Abs(currentColor.b - targetColor.b) > 0.1f)
+            {
+                currentColor.r = MoveTowards(currentColor.r, targetColor.r);
+                currentColor.g = MoveTowards(currentColor.g, targetColor.g);
+                currentColor.b = MoveTowards(currentColor.b, targetColor.b);
+                bodySprite.color = currentColor;
+                yield return null;
+            }
+            currentColorIndex = nextColorIndex;
+        }
+
+    }
+
+    private float MoveTowards(float current, float target)
+    {
+        float step = 1 / 255f;
+
+        if (current < target)
+        {
+            return Mathf.Min(current + step, target);
+        }
+        else if (current > target)
+        {
+            return Mathf.Max(current - step, target);
+        }
+
+        return target;
     }
 
 }
