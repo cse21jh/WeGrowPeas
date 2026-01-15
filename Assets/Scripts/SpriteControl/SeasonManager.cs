e9d0125c49514f7a076cae85435fcf2b36670f87
Ping-guin <stanchoi2015@gmail.com>
2026-01-16 00:23:01 +0900

    Grass Color Change (SeasonManager) Add
    - SeasonManger를 통해 배경의 풀 색상을 자연스럽게 바꾸는 기능을 추가했습니다.

--- Assets/Scripts/SpriteControl/SeasonManager.cs
+++ Assets/Scripts/SpriteControl/SeasonManager.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using DG.Tweening;
+
 
 public class SeasonManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Season currentSeason = Season.Spring;
+
+    [SerializeField] private float changeDuration = 2f;
+    [SerializeField] private Ease changeEase = Ease.InOutSine;
+
+    [SerializeField] private Material grassMat;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            currentSeason = (Season)(((int)currentSeason + 1) % 4);
+            ChangeToSeason(currentSeason);
+        }
+
+        Debug.Log(grassMat.GetFloat("_Dryness"));
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void ChangeToSeason(Season season)
     {
-        
+        switch(season)
+        {
+            case Season.Spring:
+                DOTween.To(() => 1f, x => grassMat.SetFloat("_Dryness", x), 0f, changeDuration).SetEase(changeEase);
+                grassMat.SetFloat("_Dryness", 0f);
+                break;
+            case Season.Summer:
+                grassMat.SetFloat("_Dryness", 0f);
+                break;
+            case Season.Fall:
+                DOTween.To(() => 0f, x => grassMat.SetFloat("_Dryness", x), 1f, changeDuration).SetEase(changeEase);
+                grassMat.SetFloat("_Dryness", 1f);
+                break;
+            case Season.Winter:
+                grassMat.SetFloat("_Dryness", 1f);
+                break;
+        }
     }
 }
