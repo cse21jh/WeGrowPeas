21a26dac64d249eb4a5e74d201e134d1fa4d355b
Yoon <cse21jh@korea.ac.kr>
2026-01-26 22:38:46 +0900

    Display save data info in start scene
    저장된 파일이 있을 경우 스테이지가 표시되도록 하였습니다

--- Assets/Scripts/SaveData/SaveSlotUI.cs
+++ Assets/Scripts/SaveData/SaveSlotUI.cs
@@ -1,10 +1,41 @@
 using System;
 using System.Collections;
 using System.IO;
+using TMPro;
 using UnityEngine;
 
 public class SaveSlotUI : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] slotItems;
+
+    private void Start()
+    {
+        SetSlots();
+    }
+
+    public void SetSlots()
+    {
+        for(int i = 0; i < slotItems.Length; i++)
+        {
+            var tmp = slotItems[i].GetComponentInChildren<TextMeshProUGUI>();
+
+            int slotIndex = i;
+
+            string path = SaveContext.Instance.GetSavePath(slotIndex);
+
+            if (!File.Exists(path)) tmp.text = $"저장소 {slotIndex} - 비어 있음";
+            else
+            {
+                string json = File.ReadAllText(path);
+                SaveData saveData = JsonUtility.FromJson<SaveData>(json);
+
+                tmp.text = $"저장소 {slotIndex} - Day {saveData.stage}";
+            }
+        }
+
+    }
+
     private IEnumerator DelayAction(float delay, Action action)
     {
         yield return new WaitForSecondsRealtime(delay);
