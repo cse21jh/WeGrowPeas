e34f7e54ad69d8d990e53b189bf3e39d584540be
Yoon <cse21jh@korea.ac.kr>
2026-01-27 20:37:21 +0900

    Add new features in save/load
    이미 존재하는 플레이 데이터를 선택할 때 초기화 또는 이어하기를 선택할 수 있도록 하였습니다.

--- Assets/Scripts/SaveData/SaveSlotUI.cs
+++ Assets/Scripts/SaveData/SaveSlotUI.cs
@@ -1,13 +1,17 @@
+using DG.Tweening.Plugins.Core.PathCore;
 using System;
 using System.Collections;
 using System.IO;
 using TMPro;
+using Unity.Collections;
 using UnityEngine;
 
 public class SaveSlotUI : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject[] slotItems;
+    [SerializeField] private GameObject[] slotItems;
+    [SerializeField] private GameObject savePopup;
+
+    private string path;
 
     private void Start()
     {
@@ -46,30 +50,50 @@ public class SaveSlotUI : MonoBehaviour
     {
         SaveContext.Instance.SelectSlot(slotIndex);
 
-        string path = SaveContext.Instance.CurrentSaveFilePath;
+        path = SaveContext.Instance.CurrentSaveFilePath;
 
         if(path != null && File.Exists(path)) //continue
         {
-            string json = File.ReadAllText(path);
-            SaveData saveData = JsonUtility.FromJson<SaveData>(json);
-
-            GameStartContext.SetStartType(saveData.gst);
-
-            TransitionController.instance.Transition_Out();
-            StartCoroutine(DelayAction(1.1f, () =>
-            {
-                //GameStartContext.SetStartType(GameStartType.ContinueGame);
-                SceneLoader.Instance?.LoadGardenScene();
-            }));
+            ShowSavePopup();
         }
         else //new game
         {
-            TransitionController.instance.Transition_Out();
-            StartCoroutine(DelayAction(1.1f, () =>
-            {
-                GameStartContext.SetStartType(GameStartType.NewGame);
-                SceneLoader.Instance?.LoadGardenScene();
-            }));
+            OnClickNewGame();
         }
+    }
+
+    public void OnClickNewGame()
+    {
+        TransitionController.instance.Transition_Out();
+        StartCoroutine(DelayAction(1.1f, () =>
+        {
+            GameStartContext.SetStartType(GameStartType.NewGame);
+            SceneLoader.Instance?.LoadGardenScene();
+        }));
+    }
+
+    public void OnClickContinueGame()
+    {
+        string json = File.ReadAllText(path);
+        SaveData saveData = JsonUtility.FromJson<SaveData>(json);
+
+        GameStartContext.SetStartType(saveData.gst);
+
+        TransitionController.instance.Transition_Out();
+        StartCoroutine(DelayAction(1.1f, () =>
+        {
+            //GameStartContext.SetStartType(GameStartType.ContinueGame);
+            SceneLoader.Instance?.LoadGardenScene();
+        }));
+    }
+
+    public void ShowSavePopup()
+    {
+        savePopup.SetActive(true);
+    }
+
+    public void CloseSavePopup()
+    {
+        savePopup.SetActive(false);
     }
 }
