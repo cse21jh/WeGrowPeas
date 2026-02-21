f9078cdc98c6bf3c5fcab709961c98561a9e19c1
Yoon <cse21jh@korea.ac.kr>
2026-02-22 00:41:44 +0900

    Add curse implement How-To

--- Assets/Scripts/Curse/CurseInstance/temporal/MushroomCurse.cs
+++ Assets/Scripts/Curse/CurseInstance/temporal/MushroomCurse.cs
@@ -10,10 +10,18 @@ public class MushroomCurse : CurseInstance
     public override void Activate()
     {
         Debug.Log("버섯 발생 실행");
+        //How-To
+        //Activate 함수를 구현할 때, 본 스크립트 내에 구현 로직을 모두 작성하지 말고
+        //메인 타깃이 되는 Manager나 Grid 내에 구현 후 본 스크립트에서는 호출만 해 주세요
+        //ex: Grid.ActivateMushroom
     }
 
     public override void Deactivate()
     {
         Debug.Log("버섯 발생 끝");
+        //How-To
+        //턴 종료 후 특별히 해제되어야 하는 저주일 경우 Activate와 같은 방식으로 호출
+        //ex: Grid.ClearMushroom
+        //Deactivate가 필요하지 않는 저주도 있습니다. (ex: 도둑이야! 등)
     }
 }
