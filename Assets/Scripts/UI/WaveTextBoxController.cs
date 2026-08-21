df38bc00509840699f75fa9cd65d864d896c9a94
Kim JunHa <jjun2205@gmail.com>
2026-08-21 16:29:01 +0900

    fix: Fix Bug
    저장 데이터 불러올 때, 생명 주기가 꼬여서 터지던 문제 수정

--- Assets/Scripts/UI/WaveTextBoxController.cs
+++ Assets/Scripts/UI/WaveTextBoxController.cs
@@ -4,7 +4,7 @@ public class WaveTextBoxController : MonoBehaviour
 {
     Animator anim;
 
-    private void Start()
+    private void Awake()
     {
         anim = GetComponent<Animator>();
     }
